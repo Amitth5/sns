1a7e37e3b5a4770de22bd90cdca258ef93dbd4b2
Amitth5 <amitthorat22@gmail.com>
2022-08-01 18:59:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -80,9 +80,6 @@ function transactionCalculate(){
         console.log("Total Incentives Distributed: "+ totalIncentives);
         console.log("Total Profit after deducting Incentive: "+ parseInt(totalStoreOrderValue) - parseInt(totalIncentives) );
         let sunilDeliveryCharges =  sunilOrders.reduce(function(s, f) { 
-        if(f.actual_delivery_charge < 30){
-            f.actual_delivery_charge = 30;
-        }
         s + f.actual_delivery_charge  
         }, 0)
 
