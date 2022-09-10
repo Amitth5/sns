c2ff17a3563013fbafb7eb2072d8178f962194ff
Amitth5 <amitthorat22@gmail.com>
2022-09-10 22:14:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -84,14 +84,13 @@ app.get('/transaction', function (req, res) {
 });
 
 app.post('/inserttransaction', function (req, res) {
-    console.log(req.body);
-    console.log('############################');
-    console.log(req);
+    console.log(req.body.postData);
+    
  
-    con.query("INSERT INTO order_details (delivery_id, customer_name, restaurant_name, amount, status, transaction_date) VALUES ('"+req.body.postData.delivery_boy+"', '"+req.body.postData.customer_name+"', '"+req.body.postData.resto_name+"', '"+ req.body.postData.amount +"', "+req.body.postData.status+",'"+req.body.postData.transaction_date+"'", function (err, result, fields) {
-        console.log(err);
-        console.log(result);
-    });
+//     con.query("INSERT INTO order_details (delivery_id, customer_name, restaurant_name, amount, status, transaction_date) VALUES ('"+req.body.postData.delivery_boy+"', '"+req.body.postData.customer_name+"', '"+req.body.postData.resto_name+"', '"+ req.body.postData.amount +"', "+req.body.postData.status+",'"+req.body.postData.transaction_date+"'", function (err, result, fields) {
+//         console.log(err);
+//         console.log(result);
+//     });
 
 });
 
