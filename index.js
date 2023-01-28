ddafb5b79f09a33a26a6b87cf287fe2f2ef09f37
Amitth5 <amitthorat22@gmail.com>
2023-01-28 16:21:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,15 @@ var con = mysql.createConnection({
     if(req.query.date){
         currDate = req.query.date;
     }
-
+    if(req.query.to_date && req.query.from_date){
+     transactionCalculatePeriod([req.query.from_date,req.query.to_date], 0)
+    .then((data)=>{
+        res.render('index', {"data": data,"date": currDate})
+    })
+    .catch((err)=>{
+        res.send(err);
+    })
+    }else{
     transactionCalculate(currDate, 0)
     .then((data)=>{
         res.render('index', {"data": data,"date": currDate})
@@ -40,6 +48,7 @@ var con = mysql.createConnection({
     .catch((err)=>{
         res.send(err);
     })
+    }
 })
 
 
@@ -142,6 +151,239 @@ function transactionCalculate(currDate, orderData){
     return new Promise((resolve, reject) => {
     console.log(orderData);
     con.query("SELECT orders.total, orders.sub_total, orders.actual_delivery_charge, accept_deliveries.user_id FROM orders INNER JOIN accept_deliveries ON orders.id = accept_deliveries.order_id WHERE DATE(orders.`created_at`) = '"+ currDate +"' AND orderstatus_id = 5", function (err, result, fields) {
+
+        if (err) throw err;
+
+        console.log(result);
+        let pendingAmount = 0;
+        let amitAmount = 0;
+        let refundAmount = 0;
+        let offlineOrdersAmount = 0
+        let hotelPayment = 0;
+        let cancelOrdersAmount = 0;
+        let pendingReceivedeOrdersAmount = 0;
+        if(orderData != 0){
+            let pendingOrders = _.where(orderData, {status: 1});
+            pendingAmount = pendingOrders.reduce((s, f) => s + parseInt(f.amount), 0);
+
+            let ordersPaymentAmit = _.where(orderData, {status: 5});
+            amitAmount = ordersPaymentAmit.reduce((s, f) => s + parseInt(f.amount), 0);
+
+            let refundOrders = _.where(orderData, {status: 4});
+            refundAmount = refundOrders.reduce((s, f) => s + parseInt(f.amount), 0);
+
+            let hotelOrdersPayment = _.where(orderData, {status: 3});
+            hotelPayment = hotelOrdersPayment.reduce((s, f) => s + parseInt(f.amount), 0);
+
+            let offlineOrdersPayment = _.where(orderData, {status: 2});
+            offlineOrdersAmount = offlineOrdersPayment.reduce((s, f) => s + parseInt(f.amount), 0);
+
+
+            let cancelledOrdersPayment = _.where(orderData, {status: 6});
+            cancelOrdersAmount = cancelledOrdersPayment.reduce((s, f) => s + parseInt(f.amount), 0);
+
+
+            let pendingReceivedOrdersPayment = _.where(orderData, {status: 7});
+            pendingReceivedeOrdersAmount = pendingReceivedOrdersPayment.reduce((s, f) => s + parseInt(f.amount), 0);
+
+        }
+        let totalOrderValue = Math.round(result.reduce((s, f) => s + f.total, 0));
+        let rahulReleasePayment = totalOrderValue - pendingAmount - amitAmount - refundAmount - hotelPayment - cancelOrdersAmount - pendingReceivedeOrdersAmount + offlineOrdersAmount;
+
+        let totalStoreOrderValue = result.reduce((s, f) => s + f.sub_total, 0);
+        let totalIncentives = 0;
+        let totalDeductionFromDeliveryCharges = 0;
+
+        // Sunil Total Online Orders. 1551
+        let sunilOrders = _.where(result, {user_id: 1551});
+        let sunilOrdersCount = sunilOrders.length
+        totalIncentives = totalIncentives + getIncentive(sunilOrdersCount);
+
+        //Rohit Dhanke
+        let rohitOrders = _.where(result, {user_id: 2805});
+        let rohitOrdersCount = rohitOrders.length
+        totalIncentives = totalIncentives + getIncentive(rohitOrdersCount);
+
+
+        //Satish
+        let satishOrders = _.where(result, {user_id: 3183});
+        let satishOrdersCount = satishOrders.length
+        totalIncentives = totalIncentives + getIncentive(satishOrdersCount);
+
+
+         // Subash Total Online Orders. 1697
+        let subashOrders = _.where(result, {user_id: 1697});
+        let subashOrdersCount = subashOrders.length;
+        totalIncentives =  totalIncentives + getIncentive(subashOrdersCount);
+
+        
+
+        // Shinde Total Online Orders. 1521
+        let vinayOrders = _.where(result, {user_id: 97});
+        let vinayOrdersCount = vinayOrders.length;
+
+
+        // Parmar Total Online Orders. 1084
+        let parmarShetOrders = _.where(result, {user_id: 1084});
+        let parmarShetOrdersCount = parmarShetOrders.length;
+        totalIncentives = totalIncentives + getIncentive(parmarShetOrdersCount);
+
+        
+
+        // sameer Total Online Orders. 1084
+        let sameerOrders = _.where(result, {user_id: 885});
+        let sameerOrdersCount = sameerOrders.length;
+        totalIncentives =  totalIncentives + getIncentive(sameerOrdersCount);
+
+        // Krishna Total Online Orders. 1084
+        let krishnaOrders = _.where(result, {user_id: 674});
+        let krishnaOrdersCount = krishnaOrders.length;
+
+        //Amit
+        let amitOrders = _.where(result, {user_id: 96});
+        let amitOrdersCount = amitOrders.length;
+
+
+        let totalActualDeliveryCharge = result.reduce((s, f) => s + f.actual_delivery_charge, 0);
+
+
+        resolve({
+            "totalOrder": result.length,
+            "totalOrderValue" : totalOrderValue,
+            "totalStoreOrderValue": totalStoreOrderValue,
+            "totalDeliveryCharge": totalActualDeliveryCharge,
+            "rahulReleasePayment": rahulReleasePayment,
+            "deliveryDetails": [
+                {
+                "name": "sunil",
+                "id":1551,
+                "orderCount": sunilOrdersCount,
+                "incentive": getIncentive(sunilOrdersCount),
+                "COD": sunilOrders.reduce((s, f) => s + f.total, 0),
+                "Delivery Charges": sunilOrders.reduce(function(s, f) { 
+                    if(f.actual_delivery_charge < 30){
+                        totalDeductionFromDeliveryCharges =  totalDeductionFromDeliveryCharges + (30- f.actual_delivery_charge);
+                        f.actual_delivery_charge = 30;
+                    }
+                    return s + f.actual_delivery_charge  
+                }, 0)
+                },
+                {
+                    "name": "subhash",
+                    "id":1697,
+                    "orderCount": subashOrdersCount,
+                    "incentive": getIncentive(subashOrdersCount),
+                    "COD": subashOrders.reduce((s, f) => s + f.total, 0),
+                    "Delivery Charges": subashOrders.reduce(function(s, f) { 
+                        if(f.actual_delivery_charge < 30){
+                            totalDeductionFromDeliveryCharges =  totalDeductionFromDeliveryCharges + (30- f.actual_delivery_charge);
+                            f.actual_delivery_charge = 30;
+                        }
+                        return s + f.actual_delivery_charge  
+                    }, 0),
+                },
+                {
+                    "name": "vinay",
+                    "id":97,
+                    "orderCount": vinayOrdersCount,
+                    "incentive": 0,
+                    "COD": vinayOrders.reduce((s, f) => s + f.total, 0),
+                    "Delivery Charges": vinayOrders.reduce(function(s, f) { 
+                        return s + f.actual_delivery_charge  
+                    }, 0),
+                },
+                {
+                    "name": "rahulparmar",
+                    "id":1084,
+                    "orderCount": parmarShetOrdersCount,
+                    "incentive": getIncentive(parmarShetOrdersCount),
+                    "COD": parmarShetOrders.reduce((s, f) => s + f.total, 0) - 10,
+                    "Delivery Charges": parmarShetOrders.reduce(function(s, f) { 
+                        if(f.actual_delivery_charge < 30){
+                            totalDeductionFromDeliveryCharges =  totalDeductionFromDeliveryCharges + (30- f.actual_delivery_charge);
+                            f.actual_delivery_charge = 30;
+                        }
+                        return s + f.actual_delivery_charge  
+                    }, 0),
+                },
+
+                {
+                    "name": "sameer",
+                    "id":885,
+                    "orderCount": sameerOrdersCount,
+                    "incentive": getIncentive(sameerOrdersCount),
+                    "COD": sameerOrders.reduce((s, f) => s + f.total, 0),
+                    "Delivery Charges": sameerOrders.reduce(function(s, f) { 
+                        if(f.actual_delivery_charge < 30){
+                            totalDeductionFromDeliveryCharges =  totalDeductionFromDeliveryCharges + (30- f.actual_delivery_charge);
+                            f.actual_delivery_charge = 30;
+                        }
+                        return s + f.actual_delivery_charge  
+                    }, 0),
+                },
+                {
+                    "name": "krishna",
+                    "id":674,
+                    "orderCount": krishnaOrdersCount,
+                    "incentive": 0,
+                    "COD": krishnaOrders.reduce((s, f) => s + f.total, 0),
+                    "Delivery Charges": krishnaOrders.reduce(function(s, f) { 
+                        return s + f.actual_delivery_charge  
+                    }, 0),
+                },
+                {
+                    "name": "amit",
+                    "id":96,
+                    "orderCount": amitOrdersCount,
+                    "incentive": getIncentive(amitOrdersCount),
+                    "COD": amitOrders.reduce((s, f) => s + f.total, 0),
+                    "Delivery Charges": amitOrders.reduce(function(s, f) { 
+                        return s + f.actual_delivery_charge  
+                    }, 0),
+                },
+                {
+                    "name": "bhagwan",
+                    "id":2805,
+                    "orderCount": rohitOrdersCount,
+                    "incentive": getIncentive(rohitOrdersCount),
+                    "COD": rohitOrders.reduce((s, f) => s + f.total, 0),
+                    "Delivery Charges": rohitOrders.reduce(function(s, f) { 
+                        if(f.actual_delivery_charge < 30){
+                            totalDeductionFromDeliveryCharges =  totalDeductionFromDeliveryCharges + (30- f.actual_delivery_charge);
+                            f.actual_delivery_charge = 30;
+                        }
+                        return s + f.actual_delivery_charge;  
+                    }, 0)
+                },
+                {
+                    "name": "satish",
+                    "id":3183,
+                    "orderCount": satishOrdersCount,
+                    "incentive": getIncentive(satishOrdersCount),
+                    "COD": satishOrders.reduce((s, f) => s + f.total, 0),
+                    "Delivery Charges": satishOrders.reduce(function(s, f) { 
+                        if(f.actual_delivery_charge < 30){
+                            totalDeductionFromDeliveryCharges =  totalDeductionFromDeliveryCharges + (30- f.actual_delivery_charge);
+                            f.actual_delivery_charge = 30;
+                        }
+                        return s + f.actual_delivery_charge;  
+                    }, 0)
+                }
+            ],
+            "totalIncentives": totalIncentives,
+            "totalOrderExpense": totalDeductionFromDeliveryCharges + totalIncentives,
+            "deliveryDeduction" : totalDeductionFromDeliveryCharges + totalIncentives + totalActualDeliveryCharge
+        })
+  });
+  
+});
+}
+
+
+function transactionCalculatePeriod([from_date,to_date], orderData){
+    return new Promise((resolve, reject) => {
+    console.log(orderData);
+    con.query("SELECT orders.total, orders.sub_total, orders.actual_delivery_charge, accept_deliveries.user_id FROM orders INNER JOIN accept_deliveries ON orders.id = accept_deliveries.order_id WHERE DATE(orders.`created_at`) <= '"+ from_date +"' AND DATE(orders.`created_at`) >= '"+ to_date +"' orderstatus_id = 5", function (err, result, fields) {
 
         if (err) throw err;
 
